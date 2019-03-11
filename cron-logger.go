1c265653fab4b0f1005c5cf62e52676f816cfb70
Michal Vrsansky <mivr.work@gmail.com>
2019-03-11 09:30:19 +0100

    Initial commit

--- cron-logger.go
+++ cron-logger.go
@@ -12,8 +12,8 @@ import (
 )
 
 type TeamItem struct {
-	Name    string `json:"team_name"`
-	Members uint32 `json:"members"`
+	Name      string `json:"team_name"`
+	Occupants uint32 `json:"occupants"`
 }
 
 func main() {
@@ -24,15 +24,16 @@ func main() {
 		return
 	}
 
-	handle, ferr := os.OpenFile(*config.File, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
-	if ferr != nil {
+	handle, fErr := os.OpenFile(*config.File, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if fErr != nil {
 		log.Println("Failed to open log file -> ", *config.File)
+		os.Exit(1)
 	}
 
 	var message = time.Now().UTC().String() + "\n"
 
 	defer func(msg *string) {
-		_, err := handle.Write([]byte(message))
+		_, err := handle.Write([]byte(*msg))
 		if err != nil {
 			log.Println("failed to write log file")
 		}
@@ -42,8 +43,8 @@ func main() {
 	resp, err := resty.R().Get("http://" + config.GetAddress() + "/api/v1/teams/occupants")
 	if err != nil || !resp.IsSuccess() {
 		log.Println("failed to access rest api ...")
-		message += fmt.Sprintf("rest call result: %v, err: %v\n", resp, err)
-		return
+		message += fmt.Sprintf("rest call failed, err: %v\n", err)
+		os.Exit(1)
 	}
 
 	result := make([]TeamItem, 0)
@@ -51,7 +52,7 @@ func main() {
 	if err != nil {
 		log.Println("failed to unmarshall json ...")
 		message += "failed to unmarshall REST response -> " + err.Error() + "\n"
-		return
+		os.Exit(1)
 	}
 
 	sort.Slice(result, func(i, j int) bool {
@@ -59,7 +60,7 @@ func main() {
 	})
 
 	for _, a := range result {
-		message += fmt.Sprintf("team name: %16s,  number of occupants: %d\n", a.Name, a.Members)
+		message += fmt.Sprintf("team name: %16s,  number of occupants: %d\n", a.Name, a.Occupants)
 	}
 	message += "\n"
 
